bd2c5320d9ac102a239b4c7d7e591e5b4871ac2d
NikitaKhlopin <74229659+NikitaKhlopin@users.noreply.github.com>
2022-12-27 16:19:28 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import flask
+#from gpiozero import LED
+import OPi.GPIO as GPIO
+import os
+app = flask.Flask(__name__)
+
+#GPIO.setboard()
+PORT = 5
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PORT, GPIO.OUT)
+
+@app.route("/")
+def hello_world():
+    return flask.render_template('index.html')
+    
+@app.route("/on",methods=["POST"])
+def test():
+    GPIO.output(PORT, GPIO.LOW)
+    print('on')
+    return flask.redirect('/')
+
+@app.route("/off",methods=["POST"])
+def test1():
+    GPIO.output(PORT, GPIO.HIGH)
+    #GPIO.cleanup()
+    print('off')
+    return flask.redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
